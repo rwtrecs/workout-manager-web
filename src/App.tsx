56e268d6c57b79d169fb0a9b6f69212b4f130267
Adolfo Cruz <adolfo@rewrite.com.br>
2021-03-23 15:49:47 -0400

    :nail_care: style: global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { GlobalStyle } from "./styles/global";
+
 export function App() {
   return (
     <div className="App">
       <h1>Workout Manager (Web)</h1>
+      <GlobalStyle />
     </div>
   );
 }
